ce2d7edffc6f15d087a3c8006e5b154ca19326fb
Joshua <93887296+joshuavanderbilt@users.noreply.github.com>
2023-08-27 06:35:33 -0500

    Add files via upload

---
+++ winjs.js
@@ -0,0 +1,16 @@
+// Window IDs will start at 0 and increment by 1 when a window is created.
+var winid = 0;
+
+// Make window with specified title, content and width.
+function openwindow(title, wincontent, width) {
+	winid += 1;
+	//Windows will be inserted into the element with the id "desktop"
+	var desktop = document.getElementById("desktop");
+	desktop.innerHTML += "<div class='window active' id='" + winid + "' style='position:absolute;width:" + width + "px;'><div class='title-bar'><div class='title-bar-text'>" + title + "</div><div class='title-bar-controls'><button onclick='const element = document.getElementById(" + winid + ");element.remove()' aria-label='Close'></button></div></div><div class='window-body'>" + wincontent + "</div></div>"
+	//Make window draggable
+  $( function() {
+    $( ".window" ).draggable();
+  } );
+
+}
+
