c4e18bf56e8733a14a3fb6e91724fab2c4a23310
Joshua <you@example.com>
2023-08-27 11:16:52 -0500

    Allow creating windows with specific ids

--- winjs.js
+++ winjs.js
@@ -1,17 +1,22 @@
-// Window IDs will start at 0 and increment by 1 when a window is created.
+// Window IDs will start at 0 and increment by 1 when a window is created with openwindow().
 var winid = 0;
 
-// Make window with specified title, content and width.
-function openwindow(title, wincontent, width) {
-	winid += 1;
+// Make window with specified id, title, content and width.
+function newwindow(id, title, wincontent, width) {
 	//Windows will be inserted into the element with the id "desktop"
 	var desktop = document.getElementById("desktop");
-	desktop.innerHTML += "<div class='window active' id='" + winid + "' style='position:absolute;left:40px;top:40px;width:" + width + "px;'><div class='title-bar'><div class='title-bar-text'>" + title + "</div><div class='title-bar-controls'><button onclick='const element = document.getElementById(" + winid + ");element.remove()' aria-label='Close'></button></div></div><div class='window-body'>" + wincontent + "</div></div>"
+	desktop.innerHTML += "<div class='window active' id='" + id + "' style='position:absolute;left:40px;top:40px;width:" + width + "px;'><div class='title-bar'><div class='title-bar-text'>" + title + "</div><div class='title-bar-controls'><button onclick='const element = document.getElementById(\"" + id + "\");element.remove()' aria-label='Close'></button></div></div><div class='window-body'>" + wincontent + "</div></div>"
 	//Make window draggable
   $( function() {
     $( ".window" ).draggable();
   } );
 
+}
+
+// Make window with specified title, content, and width. id is chosen automatically via winid variable.
+function openwindow(title, wincontent, width) {
+	winid += 1;
+	newwindow(winid, title, wincontent, width);
 }
 
 // Make window with specified title, iframe URL, width, and height.
